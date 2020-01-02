b82bc72af8a6b05f7e1ca9dd4aea1d11060ea41b
Nick <Nick@NickdeMacBook-Pro.local>
2020-01-02 13:14:45 +0800

    修改iOS插件

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -32,7 +32,7 @@ class _MyAppState extends State<MyApp> {
       await pluginReference.joinRoom(
           devMode: false,
           roomId: 1000001,
-          appId: 1400241682,
+          appId: 1,
           userId: "11123",
           userSig: '''eJyrVgrxCdZLrSjILEpVsjI0NTU1MjAw0AGLlqUWKVkpGekZKEH4xSnZiQUFmSlAdSYGBkYmhmYW
 RhCZzJTUvJLMtEywBkNDQyNjmJbMdKBInmNeuXNIeHhFulOaZ6lbRXB*cmiSY5RLRHlOYbGjo0*G
